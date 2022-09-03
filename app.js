d0196a8682f2258f072e579b5015bfc9c7c2ed91
Emmanuel <akaaemma@gmail.com>
2022-09-03 15:35:34 +0400

    added rounds

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let comScore = 0;
+let playerScore =0;
+let gameRound = 1;
+
 const computerPlay = () => {
     const hand = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
@@ -7,19 +11,24 @@ const computerPlay = () => {
 const compare = (pSel, cSel) => {
     if (pSel == "rock" && cSel == "paper") {
         console.log("You Lose!, paper beats rock!");
+        comScore++;
     } else if (pSel == "scissors" && cSel == "rock") {
         console.log("You Win!, rock beats scissors!");
+        comScore++;
     } else if (pSel == "paper" && cSel == "scissors") {
         console.log("You lose!, scissors beats paper!");
+        comScore++;
     } else {
         console.log(`You win, ${pSel} beats ${cSel}`);
+        playerScore++;
     }
+    gameRound++;
 }
 
 const main = () => {
     const pSel = prompt().toLowerCase();
     console.log(pSel);
-    if (pSel !== "paper" && pSel!=="rock" && pSel !=="scissors") {
+    if (pSel !== "paper" && pSel !== "rock" && pSel !== "scissors") {
         console.log("Invalid Input, input to rock, scissors or paper to play");
         main();
     }
@@ -28,7 +37,19 @@ const main = () => {
     if (pSel === cSel) {
         console.log('Draw, replay')
         main();
-    } else{
-        compare(pSel,cSel);
+    } else {
+        compare(pSel, cSel);
+    }
+}
+const play=()=>{
+    while (gameRound<5){
+        main();
+    }
+    if(playerScore==comScore){
+        main();
+    }else if(playerScore>comScore){
+        alert("You win!");
+    }else{
+        alert("computer wins!");
     }
 }
